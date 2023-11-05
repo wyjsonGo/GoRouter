430a14fce95fb1173b0373e96f8565b275dbf144
Wyjson <wyjson@qq.com>
2023-11-05 19:24:17 +0800

    注解范围@Retention CLASS 修改到 SOURCE

--- GoRouter-Annotation/src/main/java/com/wyjson/router/annotation/Route.java
+++ GoRouter-Annotation/src/main/java/com/wyjson/router/annotation/Route.java
@@ -1,13 +1,13 @@
 package com.wyjson.router.annotation;
 
 import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.CLASS;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 @Target({TYPE})
-@Retention(CLASS)
+@Retention(SOURCE)
 public @interface Route {
 
     /**
