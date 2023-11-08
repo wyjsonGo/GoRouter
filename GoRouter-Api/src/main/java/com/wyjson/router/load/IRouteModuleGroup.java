3cf8b1b2089e5508b8e0a3b14915016e02801097
Wyjson <wyjson@qq.com>
2023-11-08 18:56:11 +0800

    路由分组测试

---
+++ GoRouter-Api/src/main/java/com/wyjson/router/load/IRouteModuleGroup.java
@@ -0,0 +1,9 @@
+package com.wyjson.router.load;
+
+/**
+ * Do not use this interface.
+ * This interface is used to automatically generate routing tables and load class routing groups!
+ */
+public interface IRouteModuleGroup {
+    void load();
+}
--- module_common/src/main/java/com/wyjson/module_common/route/UserRoute.java
+++ module_common/src/main/java/com/wyjson/module_common/route/UserRoute.java
@@ -6,9 +6,13 @@ public interface UserRoute extends BaseRoute {
     String PREFIX = "/user";
 
     String SignInActivity = PREFIX + "/sign_in" + ACTIVITY_ROUTE_SUFFIX;
-    String ParamActivity = PREFIX + "/param" + ACTIVITY_ROUTE_SUFFIX;
     String CardFragment = PREFIX + "/card" + FRAGMENT_ROUTE_SUFFIX;
-    String ParamFragment = PREFIX + "/param" + FRAGMENT_ROUTE_SUFFIX;
     String UserInfoActivity = PREFIX + "/info" + ACTIVITY_ROUTE_SUFFIX;
 
+    /**
+     * 主要为了演示一个模块下多个路由分组
+     */
+    String NEW_PREFIX = "/new";
+    String ParamActivity = NEW_PREFIX + "/param" + ACTIVITY_ROUTE_SUFFIX;
+    String ParamFragment = NEW_PREFIX + "/param" + FRAGMENT_ROUTE_SUFFIX;
 }
