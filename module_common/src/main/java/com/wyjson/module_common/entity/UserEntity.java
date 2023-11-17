59919ff839004544ce3664e68acad0c1a929b723
Wyjson <wyjson@qq.com>
2023-11-17 11:06:50 +0800

    update README.md

--- module_common/src/main/java/com/wyjson/module_common/entity/UserEntity.java
+++ module_common/src/main/java/com/wyjson/module_common/entity/UserEntity.java
@@ -1,18 +1,19 @@
-package com.wyjson.module_main.event;
+package com.wyjson.module_common.entity;
 
-public class CustomEvent {
+import java.io.Serializable;
 
+public class UserEntity implements Serializable {
     public long id;
     public String name;
 
-    public CustomEvent(long id, String name) {
+    public UserEntity(long id, String name) {
         this.id = id;
         this.name = name;
     }
 
     @Override
     public String toString() {
-        return "CustomEvent{" +
+        return "UserEntity{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
