6f3da7d088408f1d988356d19243d2d3986851db
Wyjson <wyjson@qq.com>
2023-12-08 17:44:29 +0800

    支持kapt

---
+++ module_common/src/main/java/com/wyjson/module_common/route/KotlinRoute.java
@@ -0,0 +1,9 @@
+package com.wyjson.module_common.route;
+
+
+public interface KotlinRoute extends BaseRoute {
+
+    String PREFIX = "/kotlin";
+
+    String KotlinActivity = PREFIX + ACTIVITY_ROUTE_SUFFIX;
+}
