a19686b8fc4a53dcb7ebd053a5d0189afa37a62a
Wyjson <wyjson@qq.com>
2023-12-21 10:04:06 +0800

    optimize code

--- GoRouter-Gradle-Plugin/src/main/kotlin/com/wyjson/router/gradle_plugin/model/RouteHelperModel.kt
+++ GoRouter-Gradle-Plugin/src/main/kotlin/com/wyjson/router/gradle_plugin/model/RouteHelperModel.kt
@@ -1,7 +1,6 @@
 package com.wyjson.router.gradle_plugin.model
 
 data class RouteHelperModel(
-    val readme: String = "Do not delete this file, which is important to the GoRouter helper function!!!",
     val services: HashMap<String, ServiceModel> = HashMap(),
     val routes: HashMap<String, List<RouteModel>> = HashMap()
 )
